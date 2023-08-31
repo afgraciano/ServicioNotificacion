7db1397bd2c9a64ae4885ab1917476c2e871fe0f
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2023-08-31 06:42:38 -0500

    3 commit patron Fabrica o method Factory

--- src/App.java
+++ src/App.java
@@ -1,20 +1,19 @@
 /*PATRON METODO FABRICA O METHOD FACTORY que devuelve objetos llamados productos */
 
-
 import servicios.notificaciones.Canal;
 import servicios.notificaciones.Notificacion;
 import servicios.notificaciones.NotificacionFactory;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-      /*   System.out.println("Hello, World!");*/
+  public static void main(String[] args) throws Exception {
+    /* System.out.println("Hello, World!"); */
 
+    NotificacionFactory notificacionFactory = new NotificacionFactory();
 
-        NotificacionFactory notificacionFactory=new NotificacionFactory();
+    //Notificacion notificacion = notificacionFactory.crearNotificacion(Canal.WHATSAPP);
 
-        Notificacion notificacion=notificacionFactory.crearNotificacion(Canal.WHATSAPP);
+    Notificacion notificacion = notificacionFactory.crearNotificacion(Canal.SMS);
+    notificacion.notificar();
 
-        notificacion.notificar();
-
-    }
+  }
 }
